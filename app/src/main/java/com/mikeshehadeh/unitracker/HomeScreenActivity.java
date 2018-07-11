46cc15669a585ed3a8d3005ecdec82733edbe88b
Mike Shehadeh <michael.c.shehadeh@gmail.com>
2018-07-11 09:40:45 -0400

    Added checking for alerts functionality.

--- app/src/main/java/com/mikeshehadeh/unitracker/HomeScreenActivity.java
+++ app/src/main/java/com/mikeshehadeh/unitracker/HomeScreenActivity.java
@@ -1,15 +1,25 @@
 package com.mikeshehadeh.unitracker;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.Build;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 public class HomeScreenActivity extends AppCompatActivity {
 
-    private SQLiteDatabase mDatabase;
+    private SQLiteDatabase dB;
     private TermListAdapter mAdapter;
 
 
@@ -19,13 +29,113 @@ public class HomeScreenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home_screen);
 
         DBHelper dbHelper = new DBHelper(this);
-        mDatabase = dbHelper.getWritableDatabase();
+        dB = dbHelper.getWritableDatabase();
 
         configureTermsButton();
         configureCoursesButton();
         configureAdvancedButton();
         configureMentorsButton();
+        checkForAlerts();
 
+    }
+
+    private void checkForAlerts() {
+        //get cursor for all alerts with date equal to today
+        Calendar todayCal = Calendar.getInstance();
+        String todayString = parseCalToString(todayCal);
+        String whereClause = DBTables.alertTable.COLUMN_ALERT_DATETIME + "=?";
+        String[] whereArgs = new String[]{todayString};
+        Cursor c = dB.query(DBTables.alertTable.TABLE_NAME,
+                null,
+                whereClause,
+                whereArgs,
+                null,
+                null,
+                null);
+
+        //cursor not empty
+        //Iterate alert table cursor, check if courseID or assmentID is not null
+            //For assmnt ID not null, get the course designator from assmnt ID
+                //concat with alert name, append to stringbuilder
+            //For courseID not null, get course designator from course ID
+                //concat with alert name, append to stringbuilder
+        if(c.moveToFirst()) {
+            ArrayList<String> resultsAL = new ArrayList<>();
+            for(c.moveToFirst(); !c.isAfterLast(); c.moveToNext()) {
+                String assmntID = c.getString(c.getColumnIndex(DBTables.alertTable.COLUMN_ASSESSMENT_ID));
+                if(assmntID != null) {
+                    String designator = getCourseDesignatorFromAssessmentID(assmntID);
+                    String alertName = c.getString(c.getColumnIndex(DBTables.alertTable.COLUMN_ALERT_TYPE));
+                    resultsAL.add(designator + " - " + alertName);
+                }else{
+                    String courseID = c.getString(c.getColumnIndex(DBTables.alertTable.COLUMN_COURSE_ID));
+                    String designator = getCourseDesignatorFromCourseID(courseID);
+                    String alertName = c.getString(c.getColumnIndex(DBTables.alertTable.COLUMN_ALERT_TYPE));
+                    resultsAL.add(designator + " - " + alertName);
+                }
+            }
+            //Convert the arraylist to a single string, Show popup with the alerts names
+            StringBuilder sb = new StringBuilder();
+            for (String s : resultsAL) {
+                sb.append(s);
+                sb.append("\n");
+            }
+            String alertText = sb.toString();
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Today's Alerts");
+            builder.setMessage(alertText);
+            builder.setCancelable(false);
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+                }
+            });
+            builder.show();
+        }
+    }
+
+
+
+    private String getCourseDesignatorFromCourseID(String courseID) {
+        String whereClause = DBTables.courseTable.COLUMN_COURSE_ID + "=?";
+        String[] whereArgs = new String[]{courseID};
+        Cursor c = dB.query(DBTables.courseTable.TABLE_NAME, null, whereClause, whereArgs, null, null, null);
+        c.moveToFirst();
+        return c.getString(c.getColumnIndex(DBTables.courseTable.COLUMN_COURSE_DESIGNATOR));
+    }
+
+
+    private String getCourseDesignatorFromAssessmentID(String assmntID) {
+        String whereClause = DBTables.assessmentTable.COLUMN_ASSESSMENT_ID + "=?";
+        String[] whereArgs = new String[]{assmntID};
+        Cursor c = dB.query(DBTables.assessmentTable.TABLE_NAME, null,
+                whereClause, whereArgs, null, null, null);
+        c.moveToFirst();
+        String courseID = c.getString(c.getColumnIndex(DBTables.assessmentTable.COLUMN_COURSE_ID));
+
+        return getCourseDesignatorFromCourseID(courseID);
+
+    }
+
+
+
+    private String parseCalToString(Calendar lastTermEndCal) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MMM-yyyy");
+        return dateFormat.format(lastTermEndCal.getTime());
+
+    }
+
+    private Calendar parseStringToCal(String lastTermEndDate) {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MMM-yyyy");
+        try {
+            cal.setTime(dateFormat.parse(lastTermEndDate));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return cal;
     }
 
     private void configureAdvancedButton() {
